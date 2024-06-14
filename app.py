9184627f505a375018f59d41260843f33f947e03
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2024-06-15 00:15:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,21 +19,21 @@ adverse_events_data = requests.get(adverse_events_url).json()
 # Step 2: Data Preprocessing
 adverse_events_results = adverse_events_data.get("results", [])
 
-adverse_events_df = pd.json_normalize(adverse_events_results, record_path=['patient'])
+adverse_events_df = pd.DataFrame(adverse_events_results)
 
 # Data cleaning and feature engineering
-adverse_events_df = adverse_events_df.dropna(subset=["patientonsetage", "patientsex", "reaction", "drug"])
+adverse_events_df = adverse_events_df.dropna(subset=["patient.patientonsetage", "patient.patientsex", "patient.reaction", "patient.drug"])
 label_encoder = LabelEncoder()
-adverse_events_df["patientsex"] = label_encoder.fit_transform(adverse_events_df["patientsex"])
-adverse_events_df["reaction"] = adverse_events_df["reaction"].apply(lambda x: [d["reactionmeddrapt"] for d in x])
-adverse_events_df["drug_composition"] = adverse_events_df["drug"].apply(lambda x: [d["medicinalproduct"] for d in x if "medicinalproduct" in d])
-adverse_events_df["drug_indication"] = adverse_events_df["drug"].apply(lambda x: [d["drugindication"] for d in x if "drugindication" in d])
+adverse_events_df["patient.patientsex"] = label_encoder.fit_transform(adverse_events_df["patient.patientsex"])
+adverse_events_df["patient.reaction"] = adverse_events_df["patient.reaction"].apply(lambda x: [d["reactionmeddrapt"] for d in x])
+adverse_events_df["patient.drug_composition"] = adverse_events_df["patient.drug"].apply(lambda x: [d["medicinalproduct"] for d in x if "medicinalproduct" in d])
+adverse_events_df["patient.drug_indication"] = adverse_events_df["patient.drug"].apply(lambda x: [d["drugindication"] for d in x if "drugindication" in d])
 
 onehot_encoder = OneHotEncoder(sparse=False)
-X_drug_compositions = onehot_encoder.fit_transform(adverse_events_df["drug_composition"].explode().dropna().str.get_dummies().astype(bool))
-X_drug_indications = onehot_encoder.fit_transform(adverse_events_df["drug_indication"].explode().dropna().str.get_dummies().astype(bool))
-X_events = adverse_events_df[["patientonsetage", "patientsex"]].values
-y_events = adverse_events_df["reaction"].apply(lambda x: ','.join(x)).values
+X_drug_compositions = onehot_encoder.fit_transform(adverse_events_df["patient.drug_composition"].explode().dropna().str.get_dummies().astype(bool))
+X_drug_indications = onehot_encoder.fit_transform(adverse_events_df["patient.drug_indication"].explode().dropna().str.get_dummies().astype(bool))
+X_events = adverse_events_df[["patient.patientonsetage", "patient.patientsex"]].values
+y_events = adverse_events_df["patient.reaction"].apply(lambda x: ','.join(x)).values
 
 # Combine features
 X = np.concatenate([X_drug_compositions, X_drug_indications, X_events], axis=1)
